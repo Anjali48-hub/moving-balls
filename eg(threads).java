62626560fd770a79a91eb37109eba237533eae54
Anjali Mehra <67201482+Anjali48-hub@users.noreply.github.com>
2020-07-02 21:43:23 +0530

    Update eg(threads).java

--- eg(threads).java
+++ eg(threads).java
@@ -1,130 +1,138 @@
+
 import java.awt.Color;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
-public class Race implements Runnable
+
+public class THREADSDEMO1 extends Thread
+{
+    String Thname;
+    JLabel l;
+    JPanel l1,l2,l3;
+    JFrame fr;
+    public THREADSDEMO1()
     {
-      String ThreadName;
-      JLable l;
-      JPanel l1,l2,l3;
-      JFrame fr;
-      public race()
-       {
         buildGUI();
-       }
-      public void run()
-      {
-        if(Thread.currentThread().getName().equals("ObstacleA")
+    }
+    
+    public void run()
+    {
+        if(Thread.currentThread().getName().equals("aa"));
         {
-         runObstacleA();
+            aa();
         }
-        if(Thread.currentThread().getName().equals("ObstacleB")
+         if(Thread.currentThread().getName().equals("bb"));
         {
-         runObstacleB();
+            bb();
         }
-        if(Thread.currentThread().getName().equals("ObstacleC")
+         if(Thread.currentThread().getName().equals("cc"));
         {
-         runObstacleC();
+            cc();
         }
-      }
-      public void runObstacleA()
-      {
-        Random ran =new Random();
+    }
+    
+    public void aa()
+    {
+        Random ran=new Random();
         int s=ran.nextInt(1000);
         for(int i=-10;i<400;i++)
-         {
-           l1.setBounds(i,s,20,20);
-           try
-           {
-             Threads.sleep(5);
-           } 
-           catch(Exception e)
-           {
-            System.out.println(e);
-           }
-         }
-       runObstacleC();
-      }
-      public void runObstacleB()
-      {
-        Random ran =new Random();
+        {
+            l1.setBounds(i,s,20,20);
+            try 
+            {
+                Thread.sleep(5);
+            }
+            catch (InterruptedException ex)
+            {
+                
+            }
+            
+        }
+        cc();
+    }
+    public void bb()
+    {
+        Random ran=new Random();
         int r=ran.nextInt(180);
         for(int i=-10;i<400;i++)
-         {
-           l2.setBounds(i,r,20,20);
-           try
-           {
-             Threads.sleep(11);
-           } 
-           catch(Exception e)
-           {
-            System.out.println(e);
-           }
-         }
-       runObstacleA();
-      }
-      
-      public void runObstacleC()
-      {
-        Random ran =new Random();
+        {
+            l1.setBounds(i,r,20,20);
+            try 
+            {
+                Thread.sleep(5);
+            } 
+            catch (InterruptedException ex)
+            {
+                
+            }
+            
+        }
+        aa();
+    }
+        
+    public void cc()
+    {
+        Random ran=new Random();
         int m=ran.nextInt(10);
         for(int i=-10;i<400;i++)
-         {
-           l3.setBounds(i,m,20,20);
-           try
-           {
-             Threads.sleep(10);
-           } 
-           catch(Exception e)
-           {
-            System.out.println(e);
-           }
-         }
-       runObstacleB();
-      }
-      public void buildGUI()
-      {
-    	  fr=new JFrame("moving obects");
-    	  fr.setVisible(true);
-    	  fr.setSize(400,200);
-    	  fr.setLayout(null);
-    	  
-    	  l=new JLable("");
-    	  l.setBounds(10,10,400,20);
-    	  fr.add(l);
-    	  
-    	  l1=new JPanel();
-    	  l1.setSize(20,20);
-    	  l1.setBackground(color,red);
-    	  l1.setBounds(10,40,20,20);
-    	  fr.add(l1);
-    	  
-    	  l2=new JPanel();
-    	  l2.setSize(20,20);
-    	  l2.setBackground(color,blue);
-    	  l2.setBounds(10,80,20,20);
-    	  fr.add(l2);
-    	  
-    	  l3=new JPanel();
-    	  l3.setSize(20,20);
-    	  l3.setBackground(color,black);
-    	  l3.setBounds(10,120,20,20);
-    	  fr.add(l3);
-      }
-     public static void main(Sring arg[])
-     {
-    	 Race obj=new Race();
-    	 Thread Obstacle1=new Thread(obj);
-    	 Thread Obstacle2=new Thread(obj);
-    	 Thread Obstacle3=new Thread(obj);
-    	 
-      Obstacle1.setName("ObstacleA");
-      Obstacle2.setName("ObstacleB");
-      Obstacle3.setName("ObstacleC");
-      Obstacle1.start();
-      Obstacle2.start();
-      Obstacle3.start();
-     }
+        {
+            l1.setBounds(i,m,20,20);
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException ex)
+            {
+                
+            }
+            
+        }
+        bb();
     }
-      
-      
-     
+    public void buildGUI()
+    {
+        fr=new JFrame("moving objects");
+        fr.setVisible(true);
+        fr.setSize(400,400);
+        fr.setLayout(null);
+        
+        l= new JLabel("");
+        l.setBounds(10,40,20,20);
+        fr.add(l);
+        
+        l1=new JPanel();
+        l1.setSize(20,20);
+        l1.setBackground(Color.red);
+        l1.setBounds(10,40,20,20);
+                fr.add(l1);
+                
+                  
+        l2=new JPanel();
+        l2.setSize(20,20);
+        l2.setBackground(Color.blue);
+        l.setBounds(10,80,20,20);
+                fr.add(l2);
+                
+                  
+        l3=new JPanel();
+        l3.setSize(20,20);
+        l3.setBackground(Color.black);
+        l3.setBounds(10,140,20,20);
+                fr.add(l3);
+                
+    }
+    public static void main(String []a)
+    {
+        THREADSDEMO1 th=new  THREADSDEMO1();
+        Thread aa1=new Thread(th);
+        Thread bb1=new Thread(th);
+        Thread cc1=new Thread(th);
+        aa1.setName("aa");
+          bb1.setName("bb");
+            cc1.setName("cc");
+            aa1.start();
+            bb1.start();
+            cc1.start();
+                
+    }
+    
+}
